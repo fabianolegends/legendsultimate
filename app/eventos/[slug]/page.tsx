aabc901505344749101c9942fba436cffad6e6d2
Fabiano Pellenz <fabiano@ekonovaadv.com.br>
2026-07-29 09:22:10 -0300

    adiciona teste interno de inscricao no sandbox

--- app/eventos/[slug]/page.tsx
+++ app/eventos/[slug]/page.tsx
@@ -1,6 +1,12 @@
 import RegistrationClient from "./RegistrationClient";
 
-export default async function PublicEventPage({ params }: { params: Promise<{ slug: string }> }) {
-  const { slug } = await params;
-  return <RegistrationClient slug={slug}/>;
+export default async function PublicEventPage({
+  params,
+  searchParams,
+}: {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ modo?: string }>;
+}) {
+  const [{ slug }, query] = await Promise.all([params, searchParams]);
+  return <RegistrationClient slug={slug} testMode={query.modo === "teste"}/>;
 }
